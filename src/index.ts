0e0b4853f0d883676e842ffee3b6fd92347d04c4
Albert Conceição <developer.albert@outlook.com>
2023-12-11 22:11:59 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,20 @@ import express from 'express';
 
 const app = express();
 
+interface IUser {
+	name: string;
+	email: string;
+	password: string;
+}
+const user = {} as IUser;
+
+const isThisUserValid =
+	user.name &&
+	user.password &&
+	user.email &&
+	user.email.includes('@') &&
+	user.password.length > 8;
+
 app.listen(3001, () => {
-	console.log('Server is running on server');
+	console.log('Server is running on server 3001');
 });
