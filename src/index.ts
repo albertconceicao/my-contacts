cf2997127a5d86a835969449797a4a66c8f43086
Albert Conceição <developer.albert@outlook.com>
2023-12-11 22:28:16 -0300

    fix: adding file

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,6 @@ import express from 'express';
 
 const app = express();
 
-interface IUser {
-	name: string;
-	email: string;
-	password: string;
-}
-const user = {} as IUser;
-
-const isThisUserValid =
-	user.name &&
-	user.password &&
-	user.email &&
-	user.email.includes('@') &&
-	user.password.length > 8;
-
 app.listen(3001, () => {
 	console.log('Server is running on server 3001');
 });
